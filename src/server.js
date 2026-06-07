91bcbd77b59c233e4027ba864c1b876af7bcf38e
alisalhab131-lgtm <alisalhab131@gmail.com>
2026-06-08 02:36:04 +0300

    fix render crash

--- src/server.js
+++ src/server.js
@@ -1,25 +1,16 @@
-console.log("🔥 APP.JS LOADED");
+console.log("🚨 SERVER START");
 
-const express = require('express');
-const cors = require('cors');
+require('dotenv').config();
 
-const app = express();
+const app = require('./app');
 
-// middleware
-app.use(cors());
-app.use(express.json());
+// handle DB AFTER app load (safe order)
+require('./database/db');
 
-// ROUTES
-const authRoutes = require('./routes/authRoutes');
-const userRoutes = require('./routes/userRoutes');
+const PORT = process.env.PORT || 5000;
 
-// mount routes
-app.use('/api/auth', authRoutes);
-app.use('/api/users', userRoutes);
-
-// test route
-app.get('/', (req, res) => {
-  res.send('API is running...');
+app.listen(PORT, '0.0.0.0', () => {
+  console.log("=================================");
+  console.log(`🚀 SERVER RUNNING ON PORT ${PORT}`);
+  console.log("=================================");
 });
-
-module.exports = app;
