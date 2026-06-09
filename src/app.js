310ec9fc055317e8cf1ef03497a2c8ee9b0a6284
alisalhab131-lgtm <alisalhab131@gmail.com>
2026-06-09 20:41:33 +0300

    fix render crash

--- src/app.js
+++ src/app.js
@@ -1,19 +1,15 @@
+console.log("🔥 APP LOADED");
+
 const express = require('express');
 const cors = require('cors');
 
 const app = express();
 
-// middleware
 app.use(cors());
 app.use(express.json());
 
-// ROUTES
-const authRoutes = require('./routes/authRoutes');
-app.use('/api/auth', authRoutes);
-
-// test route
-app.get('/', (req, res) => {
-  res.send('API is running...');
+app.get('/test', (req, res) => {
+  res.json({ message: 'backend works' });
 });
 
 module.exports = app;
